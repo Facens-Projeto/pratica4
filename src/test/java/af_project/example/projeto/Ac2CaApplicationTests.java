2ea4a8d209184bbdeba3113fadc350a5d2aeb670
Adrian <adrianoliv.contato@gmail.com>
2025-11-16 20:39:10 -0300

    fix

--- src/test/java/af_project/example/projeto/Ac2CaApplicationTests.java
+++ src/test/java/af_project/example/projeto/Ac2CaApplicationTests.java
@@ -4,18 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import org.springframework.test.context.TestPropertySource;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:h2:mem:application-context;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-        "spring.jpa.hibernate.ddl-auto=create-drop"
-})
 class Ac2CaApplicationTests {
 
     @Test
