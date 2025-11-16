4b246063e0a0871f2ace53dabe3df33830043a77
Adrian Oliveira <63245734+adrian-oliv@users.noreply.github.com>
2025-11-16 20:04:47 -0300

    Stabilize test data sources

--- src/test/java/af_project/example/projeto/Ac2CaApplicationTests.java
+++ src/test/java/af_project/example/projeto/Ac2CaApplicationTests.java
@@ -2,12 +2,23 @@ package af_project.example.projeto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:application-context;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class Ac2CaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
